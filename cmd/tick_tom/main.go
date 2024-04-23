ef3fb30876d3f04283e19b731e9b34ee42dc730f
Tom Ecrepont <tomecrepont@gmail.com>
2024-04-23 15:08:44 +0000

    install the GH cli on the devcontainer

---
+++ cmd/tick_tom/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+
+	// Start server
+	e.Logger.Fatal(e.Start(":3000"))
+}
